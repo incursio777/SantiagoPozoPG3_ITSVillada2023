b106d8e68759c488ada9c18c4a7f1ae325a80030
incursio777 <santiagopozo2012@gmail.com>
2024-04-18 10:59:54 -0300

    feat:update

--- loqueseacarpeta/loquesea.py
+++ loqueseacarpeta/loquesea.py
@@ -4,7 +4,7 @@ def busqueda(numseleccionado, listanumeros):
             return("encontrado en la posicon", listanumeros.index(i))
             break
     if i != numseleccionado:
-        return("no encontrado") 
+        return("no encontrado ") 
 
 listanumeros = [11, 22, 33, 44, 55, 66, 77, 88, 99, 100]
 print("seleccione el numero que desea buscar")
