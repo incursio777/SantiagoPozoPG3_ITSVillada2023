b19a036798157459f4c8e4898d10b490224fc228
incursio777 <santiagopozo2012@gmail.com>
2024-08-12 10:21:02 -0300

    feat:update

--- uno_dos_tres_jango/jnago/urls.py
+++ uno_dos_tres_jango/jnago/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('hola/', hola, name='hola'),
-
+    path('template/', template),
 ]
--- uno_dos_tres_jango/jnago/views.py
+++ uno_dos_tres_jango/jnago/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import Template, Context
+import requests
 
 def hola(request):
     return HttpResponse("Hola mundo")
+
 # Create your views here.
+def template(request):
+    uri = "https://apis.datos.gob.ar/georef/api/provincias?campos=id, nombre&orden=id"
+    response1 = requests.get(uri)
+    data = response1.json()
+    provincias = data["provincias"]
+
+
+
+    t1 = open("C:/Users/Santiago/Downloads/SantiagoPozoPG3_ITSVillada2023/proyecto_ordenamiento/primertrabajo/plantillas/template1.html")
+    plantilla = Template(t1.read())
+    t1.close()
+    ctx = Context({"provincias": provincias})
+    documento = plantilla.render(ctx)
+    return HttpResponse(documento)
